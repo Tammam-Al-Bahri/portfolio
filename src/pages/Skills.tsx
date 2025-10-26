03301c6be5a457eef453941f397d21b4c2a8b42f
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2025-10-26 16:13:03 +0000

    fix: rerender ASCIIText component after small delay to fix incomplete loading on initial load

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -25,6 +25,7 @@ import { useNavigate } from "react-router-dom";
 import ASCIIText from "@/components/ASCIIText";
 import ShinyText from "@/components/ShinyText";
 import GlassSurface from "@/components/GlassSurface";
+import { useState, useEffect } from "react";
 
 export default function Skills() {
     const techLogos = [
@@ -76,12 +77,23 @@ export default function Skills() {
         "Chemistry",
     ];
     const navigate = useNavigate();
+    const [rerender, setRerender] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setRerender(true), 200);
+        return () => clearTimeout(timer);
+    }, []);
     return (
         <div className="relative overflow-auto w-screen h-screen">
             <div className="max-w-4xl mx-auto mb-12 lg:border-x-2 border-dashed bg-accent/10">
                 <div className="relative w-full h-64 md:h-96 lg:h-[300px] flex items-center justify-center overflow-hidden invert dark:invert-0">
                     <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] h-[150px] bg-white/20 dark:bg-white/10 blur-3xl rounded-full" />
-                    <ASCIIText text="Skills" enableWaves={false} asciiFontSize={8} />
+                    <ASCIIText
+                        key={rerender ? "rerendered" : "initial"}
+                        text="Skills"
+                        enableWaves={false}
+                        asciiFontSize={8}
+                    />
                 </div>
 
                 <div className="flex flex-col gap-6 px-6">
