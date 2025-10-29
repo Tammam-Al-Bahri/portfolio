225d8f4677ffa582819428bebf18b878f17efea2
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2025-10-29 05:54:02 +0000

    feat: added PixelCard component to projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,17 +1,37 @@
 import { Button } from "@/components/ui/button";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { ArrowLeft, PanelLeftIcon } from "lucide-react";
 import pages from ".";
 import { useNavigate } from "react-router-dom";
 import ASCIIText from "@/components/ASCIIText";
 import { useState, useEffect } from "react";
+import PixelCard from "@/components/PixelCard";
+import { useSidebar } from "@/components/ui/sidebar";
 
 export default function Projects() {
+    const projects = [
+        { page: pages[3], variant: "default" },
+        { page: pages[4], variant: "blue" },
+        { page: pages[7], variant: "pink" },
+        { page: pages[5], variant: "yellow" },
+        { page: pages[6], variant: "default" },
+    ] as const;
+
     const navigate = useNavigate();
     const [rerender, setRerender] = useState(false);
+
+    const { toggleSidebar } = useSidebar();
 
     useEffect(() => {
         const timer = setTimeout(() => setRerender(true), 200);
         return () => clearTimeout(timer);
+    }, []);
+
+    const [scaleClass, setScaleClass] = useState("scale-0");
+
+    useEffect(() => {
+        setTimeout(() => {
+            setScaleClass("scale-100");
+        }, 100);
     }, []);
 
     return (
@@ -26,26 +46,41 @@ export default function Projects() {
                         asciiFontSize={8}
                     />
                 </div>
-
-                <div className="flex flex-col gap-6 px-6">
-                    <div className="flex justify-between w-full max-w-4xl mx-auto mt-4 pt-4 pb-8">
-                        <Button
-                            variant="outline"
-                            onClick={() => navigate(pages[1].path, { replace: true })}
-                            className="flex items-center gap-2"
+                <div className="flex flex-wrap justify-center gap-6 px-6">
+                    {projects.map((project) => (
+                        <PixelCard
+                            variant={project.variant}
+                            className="bg-card border-border shadow-xl"
                         >
-                            <ArrowLeft />
-                            {pages[1].title}
-                        </Button>
-                        <Button
-                            variant="outline"
-                            onClick={() => navigate(pages[3].path, { replace: true })}
-                            className="flex items-center gap-2"
-                        >
-                            {pages[3].title}
-                            <ArrowRight />
-                        </Button>
-                    </div>
+                            <div
+                                className={`absolute ${scaleClass} transition-transform duration-500 ease-in-out`}
+                            >
+                                <div>{project.page.title}</div>
+                                <Button
+                                    onClick={() => navigate(project.page.path, { replace: true })}
+                                >
+                                    View
+                                </Button>
+                            </div>
+                        </PixelCard>
+                    ))}
+                </div>
+                <div className="flex justify-between w-full max-w-4xl mx-auto mt-4 px-6 pt-4 pb-8">
+                    <Button
+                        variant="outline"
+                        onClick={() => navigate(pages[1].path, { replace: true })}
+                        className="flex items-center gap-2"
+                    >
+                        <ArrowLeft />
+                        {pages[1].title}
+                    </Button>
+                    <Button
+                        variant="outline"
+                        onClick={toggleSidebar}
+                        className="flex items-center gap-2"
+                    >
+                        <PanelLeftIcon />
+                    </Button>
                 </div>
             </div>
         </div>
